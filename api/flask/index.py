921f188800966ceba974b854c627be3798c609fe
Johannes Koch <johannes.koch@student.uibk.ac.at>
2019-05-20 22:17:55 +0200

    test

--- api/flask/index.py
+++ api/flask/index.py
@@ -2,6 +2,6 @@ import datetime
 from flask import Flask, Response, __version__
 app = Flask(__name__)
 
-@app.route('/')
-def catch_all(path):
+@app.route('/api/flask/')
+def getTime(path):
     return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
