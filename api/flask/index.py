dc0cc1e2d249d89a3555a71e9a25704fc872531a
Johannes Koch <johannes.koch@student.uibk.ac.at>
2019-05-20 22:34:45 +0200

    maybe now

--- api/flask/index.py
+++ api/flask/index.py
@@ -2,6 +2,7 @@ import datetime
 from flask import Flask, Response, __version__
 app = Flask(__name__)
 
-@app.route('/api/flask/')
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
 def getTime(path):
     return Response("%s" % datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), mimetype='text/html')
