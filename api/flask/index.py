b0e05297e74bb2f4b6edf71c8206d2034888e26f
Johannes Koch <johannes.koch@student.uibk.ac.at>
2019-06-01 14:39:37 +0200

    test

--- api/flask/index.py
+++ api/flask/index.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, Response
 app = Flask(__name__)
 
-@app.route('/')
-def index(path):
-    return '{"x":123,"y":345}'
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return Response('{"x":123,"y":345}', mimetype="text/plain")
