4f05c21a26d5e1df243b8eea47223656a7555176
Johannes Koch <johannes.koch@student.uibk.ac.at>
2019-06-01 12:36:57 +0200

    flask

--- api/flask/index.py
+++ api/flask/index.py
@@ -1,7 +1,7 @@
-from flask import Flask, Response
+from flask import Flask, make_response
 app = Flask(__name__)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    return Response('{"x":123,"y":345}', mimetype="application/json")
+    return make_response('{"x":123,"y":345}', 200)
